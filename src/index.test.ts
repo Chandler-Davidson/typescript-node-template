3ec102a030301b386fef96dd481d8eddbe49462e
Chandler-Davidson <cdavidson@3-gis.com>
2020-02-25 16:16:38 -0600

    Add testing framework

---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import { sum } from "./index";
+
+test("Test 1", () => {
+  expect(sum(1, 1)).toBe(2);
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-console.log('hello');
+export const sum = (a: number, b: number) => a + b;
